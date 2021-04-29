a88e6b91a8fcc8900973d9c83d49f506e5641c77
Dobri Zhekov <zhekovdobri@gmail.com>
2021-04-30 01:04:05 +0200

    Update js.js

--- assets/js/js.js
+++ assets/js/js.js
@@ -2,13 +2,14 @@ let sidenavheader = document.querySelector('.header');
 menuLink = document.querySelectorAll('.header a');
 menuLi = document.querySelectorAll('.header li');
 addressbook = document.querySelectorAll('#addressBook a');
+iconLink = document.querySelectorAll('.mb-4 a u');
 
 (function() {
     sidenavheader.style.opacity = '0.6';
-    sidenavheader.style.width = '1350px';
+    sidenavheader.style.width = '1200px';
     sidenavheader.style.marginLeft = '40px';
     sidenavheader.style.fontFamily = 'serif';
-
+    
 
     console.log(menuLink);
 
@@ -27,6 +28,12 @@ addressbook = document.querySelectorAll('#addressBook a');
     Array.from(addressbook).forEach((link)=> {
         link.style.color = '#000'
     })
+    Array.from(iconLink).forEach((link)=> {
+        link.style.color = '#000'
+        link.style.textDecoration = 'none'  
+    })
+    
+
 
     document.getElementById("logo").style.fontWeight = "900"
     document.getElementById("logo").style.fontSize = "56px"
@@ -34,6 +41,9 @@ addressbook = document.querySelectorAll('#addressBook a');
     document.getElementById("pro").style.color = "#000"
     document.getElementById("home").style.color = "#000"
     document.body.style.backgroundColor = "#fff"
+    
+      
+
 
     console.log(menuLink)
 })()
@@ -62,15 +72,17 @@ let sideNav = document.querySelector('.sidenav');
     menuLink = document. querySelectorAll('.sidenav a');
 
 (function() {
-    sideNav.style.backgroundColor = 'grey';
+    sideNav.style.backgroundColor = '#000';
     sideNav.style.height = '100%';
     sideNav.style.width = '0';
     sideNav.style.position = 'fixed';
     sideNav.style.top = '0';
     sideNav.style.left = '0';
+    sideNav.style.opacity = '0.6'
     sideNav.style.paddingTop = '60px';
     // sideNav.style.marginTop = '500px';
     // sideNav.style.paddingLeft = '20px'; 
+    
 
     console.log(menuLink)
     Array.from(menuLink).forEach((link) => {
@@ -78,23 +90,25 @@ let sideNav = document.querySelector('.sidenav');
         link.style.textDecoration = 'none'
         link.style.color = '#fff';
         link.style.lineHeight = '36px';
-        link.style.fontSize = '24px';
-
+        link.style.fontSize = '18px';
+        link.style.fontFamily = 'Arial Narrow';
+        link.style.display = 'flexbox'
+        
     })
 })();
 
 function openSideNav() {
-    if (sideNav.style.width !== '250px') { 
-        sideNav.style.width = '250px';
-        document.querySelector('.container1').style.marginLeft = '250px';
+    if (sideNav.style.width !== '500px') { 
+        sideNav.style.width = '500px';
+        document.querySelector('.container1').style.marginLeft = '500px';
     } else { 
         sideNav.style.width = '0px';
         document.querySelector('.container1').style.marginLeft = '0px';
+        document.querySelectorAll('.bold').style.color = '#000'
     }
+
     document.querySelector('.sidenav').style.transition = '1s';
 }
-
-
 
 
 
